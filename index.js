70e82c139777f2f1b51d4d1efc7c0c27baf62092
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-10-11 11:53:22 +0200

    1. Feladat

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     let a = [];
     for (let i = 0; i < 20; i++){
-        a[i]
+        a.push(Math.round(Math.random() * (30 - 1) + 1));
     }
+    console.log(a);
+    let b = [];
+    a.forEach((e, index) => {
+        if (a[index] %5 == 0){
+            b.push(e)
+        }
+    })
+    console.log(b);
 })
